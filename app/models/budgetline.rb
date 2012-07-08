c93fb86709ea4c575215ab36b6d7c989cd6adc03
RepGenie <clayyearsley@yahoo.com>
2012-07-08 16:23:19 -0500

    Manual annotation

--- app/models/budgetline.rb
+++ app/models/budgetline.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: budgetlines
+#
+#  id             :integer         not null, primary key
+#  budget_id      :integer
+#  for_user       :integer
+#  budget_amount  :decimal(, )
+#  actual_amount  :decimal(, )
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 class Budgetline < ActiveRecord::Base
 	validates :for_user, :presence => true
 	
