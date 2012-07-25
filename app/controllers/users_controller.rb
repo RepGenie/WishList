7f2b73013085aa52ad058fb92ae52b3c10d6acee
RepGenie <clayyearsley@yahoo.com>
2012-07-25 17:31:23 -0500

    User actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,15 +42,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
+		if @user.save
+			redirect_to categories_path, :notice => 'User successfully added.'
+		else
+			render :action => "new"
+		end
   end
 
   # PUT /users/1
@@ -58,15 +54,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
+		if @user.update_attributes(params[:user])
+			redirect_to categories_path, :notice => 'User successfully updated.'
+		else
+			render :action => "edit"
+		end
   end
 
   # DELETE /users/1
